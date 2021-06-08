5c07112983fd17910ba47f76785693e1c55cc3af
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2021-06-08 17:56:01 +0200

    moved a bit to trash

--- newbuild/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.obj.d
+++
@@ -1,8 +0,0 @@
-src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.obj: \
- C:\SFML-2.5.1\src\SFML\Graphics\Vertex.cpp \
- C:/SFML-2.5.1/include/SFML/Graphics/Vertex.hpp \
- C:/SFML-2.5.1/include/SFML/Graphics/Export.hpp \
- C:/SFML-2.5.1/include/SFML/Config.hpp \
- C:/SFML-2.5.1/include/SFML/Graphics/Color.hpp \
- C:/SFML-2.5.1/include/SFML/System/Vector2.hpp \
- C:/SFML-2.5.1/include/SFML/System/Vector2.inl
--- newbuild/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.obj.d
+++
@@ -1,7 +0,0 @@
-src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.obj: \
- C:\SFML-2.5.1\src\SFML\System\Lock.cpp \
- C:/SFML-2.5.1/include/SFML/System/Lock.hpp \
- C:/SFML-2.5.1/include/SFML/System/Export.hpp \
- C:/SFML-2.5.1/include/SFML/Config.hpp \
- C:/SFML-2.5.1/include/SFML/System/NonCopyable.hpp \
- C:/SFML-2.5.1/include/SFML/System/Mutex.hpp
